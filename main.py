0d9a9521b616394711cfff133a4db34b48f2df3f
haleymhoward <haleyhoward841@gmail.com>
2024-04-05 21:41:50 -0400

    added functions to count letters and added functionality to print report of book

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
+def main():
+    path = "books/frankenstein.txt"
+    file_contents = open_book(path)
+    words = count_words(file_contents)
+    letters = count_letters(file_contents)
+    sorted_dicts = split_n_sort(letters)
+    print(f"welcome to bookbot! here is your report for {path}:")
+    print()
+    print(f"{words} words found in document")
+    print()
+
+    for item in sorted_dicts:
+        if not item["letter"].isalpha():
+            continue
+        print(f"The {item['letter']} character was found {item['num']} times")
+
+    print()
+    print("thanks for using bookbot! goodbye :)")
+
 def open_book(path):
     with open(path) as f:
         return f.read()
 
 def count_words(text):
-    count = 0
     words = text.split()
-    for word in words:
-        count += 1
-    return count
+    return len(words)
 
-def main():
-    frank_path = "books/frankenstein.txt"
-    frank_text = open_book(frank_path)
-    frank_count = count_words(frank_text)
-    print(f"{frank_count} words in document")
+def count_letters(text):
+    letters = {}
+    for c in text:
+        lower = c.lower()
+        if lower in letters:
+            letters[lower] += 1
+        else:
+            letters[lower] = 1
+    return letters
+
+def sort_on(dict):
+    return dict["num"]
+
+def split_n_sort(d):
+    list_of_dicts = [{"letter": key, "num": value} for key, value in d.items()]
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
 
 main()
-    
